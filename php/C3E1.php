e9271c4d3538583f66b2a80307b94bdb262a50d0
Alpha Diallo <alphaoumard1997@gmail.com>
2022-06-07 11:07:24 +0200

    Updates

--- php/C3E1.php
+++ php/C3E1.php
@@ -1,16 +1,119 @@
 <?php
-switch ($i)
+$commande = readline("Choisissez votre taille : M, L, XL ?");
+
+switch ($commande)
 {
-	case 0:
-		echo "i égal 0";
-		break;
-	case 1:
-		echo "i égal 1";
-		break;
-	case 2:
-		echo "i égal 2";
-		break;
+	case 1 :
+	$taille1 = " M ";
+	break;
+	case 2 :
+	$taille2 = " L ";
+	break;
+	case 3 :
+	$taille3 = " XL";
+    break;
 }
 
+$sauce0 = readline("Choisissez votre sauce");
+switch ($sauce)
+{
+	case 2 : 
+	$sauce1 = "Mayonnaise";
+	break;
+	case
+	$sauce2 ="Crevètte";
+	break;
+	case
+	$sauce3 = "Andaloise";
+	break;
+	case
+	$sauce4 = "Moutarde";
+	break;
+	case
+	$sauce5 = "Pitta";
+	break;
+	case
+	$sauce6 = "Brésiliènne";
+	break;
+	case
+	$sauce7 = "Turk";
+	break;
+	case
+	$sauce8 = "Américaine";
+	break;
+	case
+	$sauce9 = "Thaillandaise";
+	break;
+	case
+	$sauce10 = "Guinéenne";
+    break;
+}
 
+$viande0 = readline("Choisissez votre viande");
+switch ($viande)
+{
+    case 1 :
+	$viande1 = "Boeuf";
+	break;
+	case 2 :
+	$viande2 = "Pitta";
+	break;
+	case 3 :
+	$viande3 = "Boulet";
+	break;
+	case 4 :
+	$viande4 = "Poisson";
+	break;
+	case 5 :
+	$viande5 = "Kébab";
+	break;
+}
+$supplement0 = readline("Choisissez votre suppléments");
+switch ($supplement)
+{
+	case 1 :
+	$supplement1 = readline();
+	break;
+	case 2 :
+	$supplement2 = readline();
+	break; 
+	case 3 :
+	$supplement3 = readline();
+	break;
+	case 4 :
+	$supplement4 = readline();
+	break ;
+	case 5 :
+	$supplement5 = readline();
+	break;
+	case 6 :
+	$supplement6 = readline();
+	break;
+	case 7 :
+	$supplement7 = readline();
+	break;
+	case 8 :
+	$supplement8 = readline();
+	break;
+	case 9 :
+	$supplement9 = readline();
+	break;
+	case 10 :
+	$supplement10 = readline();
+    break;
+}
+
+$boisson0 = readline("Choisissez votre taille de boisson !");
+switch ($boisson)
+{
+	case 1 :
+	$boisson1 = readline();
+	break;
+	case 2 :
+	$boisson2 = readline();
+	break;
+	case 3 :
+	$boisson3 = readline();
+	break;
+}
 ?>
